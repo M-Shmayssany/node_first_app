f79723a6d23beb21cfbf962dfd541fd939449ef0
M-Shmayssany <m.shmayssany@gmail.com>
2020-09-30 12:28:25 +0200

    add delete function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const jsonfile = require('jsonfile');
-
+const file_path = './DB/users.json';
 
 module.exports = app => {
 
@@ -7,24 +7,42 @@ module.exports = app => {
         console.log('Fetching all users');
 
         //jsonfile reading
-        jsonfile.readFile('./DB/users.json', (err, content) => {
+        jsonfile.readFile(file_path, (err, content) => {
 
             // sending file contents back to sender
             res.send(content);
         })
     });
 
-    
+
     app.post("/users/new", (req, res) => {
         let email = req.body.email;
         let username = req.body.username;
 
-        jsonfile.readFile("./DB/users.json", (err, content)=>{
+        jsonfile.readFile(file_path, (err, content)=>{
             content.push({ email: email, username: username });
 
             console.log("Added " + email + " to DB");
 
-            jsonfile.writeFile("./DB/users.json", content, (err)=>{
+            jsonfile.writeFile(file_path, content, (err)=>{
+                console.log(err);
+            });
+            res.sendStatus(200);
+        });
+    });
+
+    app.delete("/users/destroy", (req, res)=>{
+        let email = req.body.email;
+        jsonfile.readFile(file_path, (err, content)=>{
+            
+            for (let i = content.length - 1; i >= 0; i--){
+                
+                if(content[i].email === email){
+                    console.log("Removing " + content[i].email + " from DB");
+                    content.pop(i);
+                }
+            }
+            jsonfile.writeFile(file_path, content,(err)=>{
                 console.log(err);
             });
             res.sendStatus(200);
