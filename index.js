260765377526a738e13df396e9e49dcb8a7b9f45
M-Shmayssany <m.shmayssany@gmail.com>
2020-09-30 11:28:04 +0200

    add post function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //mport the NPM dependancy package
 const express = require("express");
+const bodyParser  = require("body-parser");
 //initialise express() inside and write to the app variable
 const app = express();
+
+app.use(bodyParser.json());
 //import route module and pass your app
 require("./routes/userRoutes")(app);
 //choose what port on which to run the server
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,33 @@
 const jsonfile = require('jsonfile');
 
+
 module.exports = app => {
 
-        app.get("/users", (req, res) => {
-            let users;
-            jsonfile.readFile('./DB/users.json', (err, data) => {
-                res.send(data);
-            })
+    app.get("/users", (req, res) => {
+        console.log('Fetching all users');
+
+        //jsonfile reading
+        jsonfile.readFile('./DB/users.json', (err, content) => {
+
+            // sending file contents back to sender
+            res.send(content);
+        })
+    });
+
+    
+    app.post("/users/new", (req, res) => {
+        let email = req.body.email;
+        let username = req.body.username;
+
+        jsonfile.readFile("./DB/users.json", (err, content)=>{
+            content.push({ email: email, username: username });
+
+            console.log("Added " + email + " to DB");
+
+            jsonfile.writeFile("./DB/users.json", content, (err)=>{
+                console.log(err);
+            });
+            res.sendStatus(200);
         });
+    });
 }
